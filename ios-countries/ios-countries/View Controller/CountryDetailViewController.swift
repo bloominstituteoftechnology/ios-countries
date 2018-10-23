3ef45852ef664df02a86c478eb69f67a4a522b4a
Conner Alegre <cga0@pm.me>
2018-10-23 16:22:52 -0600

    Setup CountryDetailViewController to accept Country

--- ios-countries/ios-countries/View Controller/CountryDetailViewController.swift
+++ ios-countries/ios-countries/View Controller/CountryDetailViewController.swift
@@ -9,22 +9,21 @@
 import UIKit
 
 class CountryDetailViewController: UIViewController {
+    
+    // MARK: - Properties
+    var country: Country? {
+        didSet {
+            updateViews()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private
+    private func updateViews() {
+        guard let country = country, isViewLoaded else { return }
+        title = country.name
     }
-    */
-
 }
