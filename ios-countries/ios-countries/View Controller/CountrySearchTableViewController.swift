4522036ae1a3340cdd04e6ed06443a3f57377c12
Conner Alegre <cga0@pm.me>
2018-10-23 16:28:23 -0600

    Pass Country to Detail via Segue

--- ios-countries/ios-countries/View Controller/CountrySearchTableViewController.swift
+++ ios-countries/ios-countries/View Controller/CountrySearchTableViewController.swift
@@ -36,16 +36,18 @@ class CountrySearchTableViewController: UITableViewController {
         
         return cell
     }
-
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ShowDetailSegue" {
+            let navigationViewController = segue.destination as! UINavigationController
+            let detailViewController = navigationViewController.topViewController as! CountryDetailViewController
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let country = countryController.countries[indexPath.row]
+                detailViewController.country = country
+            }
+        }
     }
-    */
 
 }
 
