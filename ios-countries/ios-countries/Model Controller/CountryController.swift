b2f15fc55d32fa09061126db09a6e189403fd9a5
Conner Alegre <cga0@pm.me>
2018-10-23 15:47:08 -0600

    Implement search into CountryController

--- ios-countries/ios-countries/Model Controller/CountryController.swift
+++ ios-countries/ios-countries/Model Controller/CountryController.swift
@@ -9,5 +9,30 @@
 import Foundation
 
 class CountryController {
+    func performSearch(for searchTerm: String, completion: @escaping () -> Void) {
+        let url = baseURL.appendingPathComponent(searchTerm.lowercased())
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error GET countries: \(error)")
+                return
+            }
+            guard let data = data else {
+                completion()
+                return
+            }
+            do {
+                let jsonDecoder = JSONDecoder()
+                let searchResults = try jsonDecoder.decode(Country.self, from: data)
+            } catch {
+                NSLog("Error decoding \(data) into object, error: \(error)")
+            }
+            completion()
+        }
+    }
     
+    let baseURL = URL(string: "https://restcountries.eu/rest/v2/name/")!
+    let countries: [Country] = []
 }
