ef668e90f7976041b04e5c457f934c00c307e83d
Moin Uddin <U_Moin75@yahoo.com>
2018-12-04 21:50:48 -0500

    implement network request method to fetch all countries

---
+++ Countries/Countries/Country.swift
@@ -0,0 +1,20 @@
+//
+//  Country.swift
+//  Countries
+//
+//  Created by Moin Uddin on 12/4/18.
+//  Copyright © 2018 Moin Uddin. All rights reserved.
+//
+
+import UIKit
+
+
+struct Country {
+    let name: String
+    let region: String
+    let capital: String
+    let population: Int 
+    let currencies: [String]
+    let languages: [String]
+    let flag: UIImage
+}
