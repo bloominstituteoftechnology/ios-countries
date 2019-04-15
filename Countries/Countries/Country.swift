42e6aaab787bcf0586121d9538316385af757b47
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-04-15 18:44:06 -0400

    update UI and add flag  folder

--- Countries/Countries/Country.swift
+++ Countries/Countries/Country.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+struct CountryElement: Codable {
+    let name: String
+    let capital: String
+    let region: String
+    let population: Int
+    let currencies: [Currency]
+    let languages: [Language]
+    let flag: String
+}
+
+struct Currency: Codable {
+    let code, name: String
+    let symbol: String?
+}
+
+struct Language: Codable {
+    let iso6391, iso6392, name, nativeName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case iso6391 = "iso639_1"
+        case iso6392 = "iso639_2"
+        case name, nativeName
+    }
+}
+
+
+
