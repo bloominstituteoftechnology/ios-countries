a90cce444dede9acfae35ab8cf25907bd2b026e1
Frulwinn <frulwinn@gmail.com>
2019-04-15 20:48:31 -0500

    added controller

--- Countries/Countries/Model/CountryController.swift
+++ Countries/Countries/Model/CountryController.swift
@@ -10,5 +10,42 @@ import UIKit
 
 class CountryController {
     
+    //MARK: - Properties
+    var countries: [Country] = []
     
+    let baseURL = URL(string: "https://restcountries.eu/rest/v2/name/")!
+
+    
+    func fetchCountries(countryName: String, completion: @escaping (Error?) -> Void) {
+        let url = baseURL.appendingPathComponent(countryName)
+        
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            
+            if let error = error {
+                NSLog("Error found while fetching for countries: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No countries data found.")
+                completion(error)
+                return
+            }
+            
+            do {
+                let jsonDecoder = JSONDecoder()
+                let decodedCountries = try jsonDecoder.decode([Country].self, from: data)
+                self.countries = decodedCountries
+                
+            } catch {
+                NSLog("could not decode data.")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }
+        dataTask.resume()
+    }
 }
