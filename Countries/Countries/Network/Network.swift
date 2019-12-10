bdbbfa5442123abefea877a5d95fc2905f8e94b3
Michael R.F <mrflowers22@uwalumni.com>
2019-12-09 19:42:05 -0800

    created the fetch country function in network file

--- Countries/Countries/Network/Network.swift
+++ Countries/Countries/Network/Network.swift
@@ -10,6 +10,42 @@ import Foundation
 
 
 class Network {
+    static let shared = Network()
+//    var countries: [Country] = []
+     fileprivate let baseURL = URL(string: "https://restcountries.eu/rest/v2/name")!
     
-    static func getCountryFrom(searchTerm: String, completion: @escaping ([Flag]))
+      func getCountryFrom(searchTerm: String, completion: @escaping (Country?, Error?) -> Void){
+        //append the baseURL with the search term
+        let url = baseURL.appendingPathComponent(searchTerm)
+        
+        //we are getting so we just call urlsession
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let response = response as? HTTPURLResponse {
+                print("Response: \(response.statusCode)")
+            }
+            
+            if let error = error {
+                print("Error in file: \(#file) in the body of the function: \(#function)\n on line: \(#line)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)\n")
+                completion(nil, error)
+                return
+            }
+            
+            guard let data = data else {
+                print("Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                completion(nil, NSError(domain: "Error in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n", code: 422))
+                return
+            }
+            
+            let jD = JSONDecoder()
+            do {
+                let country =  try jD.decode(Country.self, from: data)
+                print("success getting country back in file: \(#file), in the body of the function: \(#function) on line: \(#line)\n")
+                completion(country, nil)
+            } catch  {
+                print("Error in: \(#function)\n Readable Error: \(error.localizedDescription)\n Technical Error: \(error)")
+                completion(nil, error)
+                return
+            }
+        }.resume()
+    }
 }
